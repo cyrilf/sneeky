65139bbb200f2d47fdb41f6a61c265869a8807de
Neil <neil.rosenstech@gmail.com>
2012-12-24 01:53:41 -0500

    Tired

--- engine.js
+++ engine.js
@@ -2,12 +2,10 @@ var sneeky = function() {
 
     var canvas = document.getElementById( 'canvas' );
     var ctx = canvas.getContext( '2d' );
-    var unit = 11;
-    var trailUnit = 2;
-    var trailOffset = Math.floor(unit / 2);
+    var unit = 1;
 
     var tock = true;
-    var tickSpeed = 40;
+    var tickSpeed = 5;
 
     var keys = [ [ 87, 38 ], [ 39, 68 ], [ 40, 83 ], [ 37, 65 ] ];
     var directions = {
@@ -23,7 +21,7 @@ var sneeky = function() {
     trails.unshift( randomCoords() );
 
     var mainColor = '#2186ed';
-    var trailColor = 'rgba(103, 228, 252, 0.7)';
+    var trailColor = 'rgba(103, 228, 252, 1)';
 
     function randomCoords() {
         var x = Math.round( Math.round( Math.random() * ( canvas.width  - ( unit ) ) ) / unit ) * unit;
@@ -56,73 +54,26 @@ var sneeky = function() {
 
     function updateCoords() {
         var coords = trails[0];
-
-        if( direction === directions.UP ) {
-            coords.y -= unit;
-        } else if( direction === directions.RIGHT ) {
-            coords.x += unit;
-        } else if( direction === directions.DOWN ) {
-            coords.y += unit;
-        } else if( direction === directions.LEFT ) {
-            coords.x -= unit;
+        switch (direction) {
+            case directions.RIGHT: coords.x++; break;
+            case directions.LEFT: coords.x--; break;
+            case directions.UP: coords.y--; break;
+            case directions.DOWN: coords.y++; break;
         }
-
-        if( coords.x < 0 ) {
-            coords.x = canvas.width - unit;
-        } else if( coords.x + unit > canvas.width ) {
-            coords.x = 0;
-        }
-
-        if( coords.y < 0 ) {
-            coords.y = canvas.height - unit;
-        } else if( coords.y + unit > canvas.height ) {
-            coords.y = 0;
-        }
-        //console.log(coords );
-        //The following bug
-        //trails.unshift( coords );
-        //But this one works ! WTF ? #answer ?
         trails.unshift( { x : coords.x, y : coords.y } );
     }
 
-    function drawTrail () {
-        //Draw the trails
+    function drawTrail() {
         if (trails.length > 2) {
-            ctx.clearRect(trails[2].x, trails[2].y, unit, unit);
+            ctx.shadowBlur = 5;
+            ctx.shadowColor = trailColor;
             ctx.strokeStyle = trailColor;
-            if (Math.abs(trails[0].x - trails[2].x) <= 20 && Math.abs(trails[0].y - trails[2].y) <= 20 && trails[0].x != 0 && trails[0].y != 0) {
-                ctx.moveTo(trails[0].x + trailOffset, trails[0].y + trailOffset);
-                ctx.lineTo(trails[2].x + trailOffset, trails[2].y + trailOffset);
-            } else if (Math.abs(trails[0].x - trails[2].x) > 20) {
-                if (trails[0].x == 0) {
-                    ctx.moveTo(canvas.width, trails[0].y + trailOffset);
-                    ctx.lineTo(trails[2].x + trailOffset, trails[2].y + trailOffset);
-                    ctx.moveTo(trails[0].x + trailOffset, trails[0].y + trailOffset);
-                    ctx.lineTo(0, trails[2].y + trailOffset);
-                } else {
-                    ctx.moveTo(trails[2].x + trailOffset + unit, trails[2].y + trailOffset);
-                    ctx.lineTo(0, trails[2].y + trailOffset);
-                    ctx.moveTo(trails[0].x + trailOffset, trails[0].y + trailOffset);
-                    ctx.lineTo(canvas.width, trails[2].y + trailOffset);
-                }
-            } else if (Math.abs(trails[0].y - trails[2].y) > 20) {
-                if (trails[0].y == 0) {
-                    ctx.moveTo(trails[0].x + trailOffset, canvas.height);
-                    ctx.lineTo(trails[2].x + trailOffset, trails[2].y + trailOffset);
-                    ctx.moveTo(trails[0].x + trailOffset, trails[0].y + trailOffset);
-                    ctx.lineTo(trails[2].x + trailOffset, 0);
-                } else {
-                    ctx.moveTo(trails[2].x + trailOffset, trails[2].y + trailOffset + unit);
-                    ctx.lineTo(trails[2].x + trailOffset, 0);
-                    ctx.moveTo(trails[0].x + trailOffset, trails[0].y + trailOffset);
-                    ctx.lineTo(trails[2].x + trailOffset, canvas.height);
-                }
-            }
+            ctx.beginPath();
+            ctx.moveTo(trails[0].x, trails[0].y);
+            ctx.lineTo(trails[2].x, trails[2].y);
+            ctx.closePath();
             ctx.stroke();
         }
-        //Draw the "head"
-        ctx.fillStyle = mainColor;
-        ctx.fillRect(trails[0].x, trails[0].y, unit, unit);
     }
 
     function tick() {

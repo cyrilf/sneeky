40ecb36f0945f44a2c3a72f27cc7abed7bab4997
cyrilf <cyrilf7x@gmail.com>
2014-04-28 21:45:08 +0200

    Player refactored to Prototype   - more consistency in code

--- server/player/player.js
+++ server/player/player.js
@@ -9,120 +9,105 @@ var collisionEngine = require('../collisionEngine');
  * @param {Object} config configuration for the player
  */
 var Player = function(config) {
-  var id        = config.id,
-      color     = config.color,
-      canvas    = config.canvas,
-      unit      = config.unit,
-      origin    = config.origin,
-      trails    = [],
-      direction,
-      score     = 0,
-      isPlaying = false;
+  this.id        = config.id;
+  this.color     = config.color;
+  this.canvas    = config.canvas;
+  this.unit      = config.unit;
+  this.origin    = config.origin;
+  this.direction = directions.inverse(this.origin);
+  this.trails    = [];
+  this.score     = 0;
+  this.isPlaying = false;
 
   // TODO @remove this
-  canvas = {
+  this.canvas = {
     width: 500,
     height: 500
   };
-  unit = 3;
+  this.unit = 3;
+};
 
-  /**
-   * Init the player to his default option
-   *   (when a new game start)
-   * @param  {Boolean} gameIsOn true if the game is on
-   */
-  var init = function(gameIsOn) {
-    this.direction = directions.inverse(origin);
-    this.trails    = [];
-    this.trails.unshift(getStartPosition(origin));
-    // This player is not playing if the game is on
-    // (occurs when a new player join a game already on,
-    //  he have to wait for the end of it)
-    this.isPlaying = !gameIsOn;
-  };
+/**
+ * Init the player to his default option
+ *   (when a new game start)
+ * @param  {Boolean} gameIsOn true if the game is on
+ */
+Player.prototype.init = function(gameIsOn) {
+  this.direction = directions.inverse(this.origin);
+  this.trails    = [];
+  this.trails.unshift(this.getStartPosition(this.origin));
+  // This player is not playing if the game is on
+  // (occurs when a new player join a game already on,
+  //  he have to wait for the end of it)
+  this.isPlaying = !gameIsOn;
+};
 
-  /**
-   * get the player start position on the grid
-   * @param  {Int} start  a directions.ENUM
-   * @return {Object}     the start position (x and y)
-   */
-  var getStartPosition = function(start) {
-    var p = { x : 0, y : 0 };
-    var c = canvas,
-    u = unit;
-    switch( start ) {
-      case directions.UP :
-        p.x = Math.round( ( ( c.width / 2 ) - 2 * u ) / u ) * u;
-        p.y = 10 * u;
-        break;
-      case directions.RIGHT :
-        p.x = c.width - ( 10 * u );
-        p.y = Math.round( ( ( c.height / 2 ) - 2 * u ) / u ) * u;
-        break;
-      case directions.DOWN :
-        p.x = Math.round( ( ( c.width / 2 ) + 2 * u ) / u ) * u;
-        p.y = c.height - ( 10 * u );
-        break;
-      case directions.LEFT :
-        p.x = 10 * u;
-        p.y = Math.round( ( ( c.height / 2 ) + 2 * u ) / u ) * u;
-        break;
+/**
+ * get the player start position on the grid
+ * @param  {Int} start  a directions.ENUM
+ * @return {Object}     the start position (x and y)
+ */
+Player.prototype.getStartPosition = function(start) {
+  var p = { x : 0, y : 0 };
+  var c = this.canvas,
+  u = this.unit;
+  switch( start ) {
+    case directions.UP :
+      p.x = Math.round( ( ( c.width / 2 ) - 2 * u ) / u ) * u;
+      p.y = 10 * u;
+      break;
+    case directions.RIGHT :
+      p.x = c.width - ( 10 * u );
+      p.y = Math.round( ( ( c.height / 2 ) - 2 * u ) / u ) * u;
+      break;
+    case directions.DOWN :
+      p.x = Math.round( ( ( c.width / 2 ) + 2 * u ) / u ) * u;
+      p.y = c.height - ( 10 * u );
+      break;
+    case directions.LEFT :
+      p.x = 10 * u;
+      p.y = Math.round( ( ( c.height / 2 ) + 2 * u ) / u ) * u;
+      break;
+    }
+
+    return p;
+};
+
+/**
+ * Move a player if necessary (if no collision)
+ * @param  {[Player]} players list of all player (for check collision) (to removed)
+ * @return {Promise(Boolean)} true if no collision occured
+ *                            false otherwise
+ */
+Player.prototype.move = function(players) {
+  var deferred = Q.defer();
+
+  collisionEngine.hasCollision(this, players, this.canvas, this.unit).then(function(collision) {
+    if(! collision) {
+      var x = this.trails[0].x,
+      y = this.trails[0].y,
+      d = this.direction;
+
+      if(d === directions.UP) {
+        y -= this.unit;
+      } else if(d === directions.RIGHT) {
+        x += this.unit;
+      } else if(d === directions.DOWN) {
+        y += this.unit;
+      } else if(d === directions.LEFT) {
+        x -= this.unit;
       }
 
-      return p;
-  };
+      var trail = { x : x, y : y };
 
-  /**
-   * Move a player if necessary (if no collision)
-   * @param  {[Player]} players list of all player (for check collision) (to removed)
-   * @return {Promise(Boolean)} true if no collision occured
-   *                            false otherwise
-   */
-  var move = function(players) {
-    var deferred = Q.defer();
+      this.trails.unshift(trail);
+      deferred.resolve(true);
+    } else {
+      deferred.resolve(false);
+    }
+  });
 
-    collisionEngine.hasCollision(this, players, this.canvas, this.unit).then(function(collision) {
-      if(! collision) {
-        var x = this.trails[0].x,
-        y = this.trails[0].y,
-        d = this.direction;
-
-        if(d === directions.UP) {
-          y -= unit;
-        } else if(d === directions.RIGHT) {
-          x += unit;
-        } else if(d === directions.DOWN) {
-          y += unit;
-        } else if(d === directions.LEFT) {
-          x -= unit;
-        }
-
-        var trail = { x : x, y : y };
-
-        this.trails.unshift(trail);
-        deferred.resolve(true);
-      } else {
-        deferred.resolve(false);
-      }
-    });
-
-    return deferred.promise;
-  };
-
-  return {
-    init             : init,
-    getStartPosition : getStartPosition,
-    move             : move,
-    id               : id,
-    canvas           : canvas,
-    unit             : unit,
-    color            : color,
-    origin           : origin,
-    trails           : trails,
-    direction        : direction,
-    score            : score,
-    isPlaying        : isPlaying
-  };
+  return deferred.promise;
 };
 
 module.exports = Player;
